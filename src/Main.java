3a8f896b5def487c81e3f1b6601bb8e9be14d0d6
Tamara Sibileva <tomav93@mail.ru>
2024-12-11 16:45:56 +1200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int openingBalance = 100; // начальный счёт
+        int topUp = 1300; // сумма пополнения счёта
+        int b = 100; // количество рублей для одного бонусного рубля
+        int minTopUp = 1000; // минимальная сумма пополнения для начисления бонусов
+        int bonus;
+
+        if (topUp > minTopUp) {
+            bonus = topUp / b;
+        } else {
+            bonus = 0;
+        }
+        int balance = openingBalance + topUp + bonus; // итоговый счёт
+        System.out.println("Итоговый счёт: " + balance + " Бонусные рубли: " + bonus);
+    }
+}
